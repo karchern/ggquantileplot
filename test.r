c9c84a5b57b6a25eda8a781f35002439faa66d72
Nicolai Karcher <karchernic@gmail.com>
2023-12-21 14:49:44 +0100

    adding image for readme

--- test.r
+++ test.r
@@ -3,8 +3,10 @@ library(grid)
 library(ggplot2)
 library(ggquantileplot)
 
-ggplot(data = ToothGrowth, aes(x = as.factor(dose), y = len)) +
-    geom_quantileplot(aes(fill = as.factor(supp))) +
-    geom_point(aes(color = as.factor(supp), group = supp), position = position_jitterdodge()) +
+pQuantileplot <- ggplot(data = ToothGrowth, aes(x = as.factor(dose), y = len)) +
+    geom_quantileplot(aes(fill = as.factor(supp)), quantilesP = c(0.5, 0.7, 0.9, 1)) +
     scale_fill_manual(values = c("OJ" = "red", "VC" = "blue")) +
-    scale_color_manual(values = c('OJ' = "red", "VC" = "blue"))
+    scale_color_manual(values = c("OJ" = "red", "VC" = "blue")) +
+    theme_classic()
+
+ggsave(plot = pQuantileplot, filename = "/Users/karcher/ggquantileplot/test.png")

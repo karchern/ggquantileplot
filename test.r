f1ea41d38d4546e4a63cfc772472d394aa5d8978
Nicolai Karcher <karchernic@gmail.com>
2023-12-21 09:06:09 +0100

    Stat ported but not quantilePlots not yet implemented

--- test.r
+++ test.r
@@ -4,6 +4,7 @@ library(ggplot2)
 # These functions need to be loaded for some internal functions in GeomQuantilePlot
 library(rlang)
 library(ggforce)
+library(vctrs)
 
 # Some more coded that needed to be copied manually. I don't know why these functions are not exposed and I'm not even sure where they come from
 ggname <- function(prefix, grob) {
@@ -31,10 +32,12 @@ check_linewidth <- function(data, name) {
   data
 }
 
+is_mapped_discrete <- function (x) inherits(x, "mapped_discrete")
+
 data_frame0 <- function(...) data_frame(..., .name_repair = "minimal")
 
 geom_quantileplot <- function(mapping = NULL, data = NULL,
-                         stat = "boxplot", position = "dodge2",
+                         stat = StatQuantileplot, position = "dodge2",
                          ...,
                          outlier.colour = NULL,
                          outlier.color = NULL,
@@ -87,6 +90,92 @@ geom_quantileplot <- function(mapping = NULL, data = NULL,
     )
   )
 }
+
+StatQuantileplot <- ggproto("StatBoxplot", Stat,
+  required_aes = c("y|x"),
+  non_missing_aes = "weight",
+  # either the x or y aesthetic will get dropped during
+  # statistical transformation, depending on the orientation
+  dropped_aes = c("x", "y", "weight"),
+  setup_data = function(self, data, params) {
+    data <- flip_data(data, params$flipped_aes)
+    data$x <- data$x %||% 0
+    data <- remove_missing(
+      data,
+      na.rm = params$na.rm,
+      vars = "x",
+      name = "stat_boxplot"
+    )
+    flip_data(data, params$flipped_aes)
+  },
+
+  setup_params = function(self, data, params) {
+    params$flipped_aes <- has_flipped_aes(data, params, main_is_orthogonal = TRUE,
+                                          group_has_equal = TRUE,
+                                          main_is_optional = TRUE)
+    data <- flip_data(data, params$flipped_aes)
+
+    has_x <- !(is.null(data$x) && is.null(params$x))
+    has_y <- !(is.null(data$y) && is.null(params$y))
+    if (!has_x && !has_y) {
+      cli::cli_abort("{.fn {snake_class(self)}} requires an {.field x} or {.field y} aesthetic.")
+    }
+
+    params$width <- params$width %||% (resolution(data$x %||% 0) * 0.75)
+
+    if (!is_mapped_discrete(data$x) && is.double(data$x) && !has_groups(data) && any(data$x != data$x[1L])) {
+      cli::cli_warn(c(
+        "Continuous {.field {flipped_names(params$flipped_aes)$x}} aesthetic",
+        "i" = "did you forget {.code aes(group = ...)}?"
+      ))
+    }
+
+    params
+  },
+
+  extra_params = c("na.rm", "orientation"),
+
+  compute_group = function(data, scales, width = NULL, na.rm = FALSE, coef = 1.5, flipped_aes = FALSE) {
+    data <- flip_data(data, flipped_aes)
+    qs <- c(0, 0.25, 0.5, 0.75, 1)
+
+    # if (!is.null(data$weight)) {
+    #     mod <- quantreg::rq(y ~ 1, weights = weight, data = data, tau = qs)
+    #     stats <- as.numeric(stats::coef(mod))
+    # } else {
+    stats <- as.numeric(stats::quantile(data$y, qs))
+    #browser()
+    names(stats) <- c("ymin", "lower", "middle", "upper", "ymax")
+    iqr <- diff(stats[c(2, 4)])
+
+    outliers <- data$y < (stats[2] - coef * iqr) | data$y > (stats[4] + coef * iqr)
+    if (any(outliers)) {
+      stats[c(1, 5)] <- range(c(stats[2:4], data$y[!outliers]), na.rm = TRUE)
+    }
+
+    if (vec_unique_count(data$x) > 1)
+      width <- diff(range(data$x)) * 0.9
+
+    df <- data_frame0(!!!as.list(stats))
+    df$outliers <- list(data$y[outliers])
+
+    if (is.null(data$weight)) {
+      n <- sum(!is.na(data$y))
+    } else {
+      # Sum up weights for non-NA positions of y and weight
+      n <- sum(data$weight[!is.na(data$y) & !is.na(data$weight)])
+    }
+
+    df$notchupper <- df$middle + 1.58 * iqr / sqrt(n)
+    df$notchlower <- df$middle - 1.58 * iqr / sqrt(n)
+
+    df$x <- if (is.factor(data$x)) data$x[1] else mean(range(data$x))
+    df$width <- width
+    df$relvarwidth <- sqrt(n)
+    df$flipped_aes <- flipped_aes
+    flip_data(df, flipped_aes)
+  }
+)
 
 #' @rdname ggplot2-ggproto
 #' @format NULL
@@ -160,16 +249,16 @@ GeomQuantileplot <- ggproto("GeomQuantileplot", Geom,
       group = data$group
     )
     
-    whiskers <- data_frame0(
-      x = c(data$x, data$x),
-      xend = c(data$x, data$x),
-      y = c(data$upper, data$lower),
-      yend = c(data$ymax, data$ymin),
-      alpha = c(NA_real_, NA_real_),
-      !!!common,
-      .size = 2
-    )
-    whiskers <- flip_data(whiskers, flipped_aes)
+    # whiskers <- data_frame0(
+    #   x = c(data$x, data$x),
+    #   xend = c(data$x, data$x),
+    #   y = c(data$upper, data$lower),
+    #   yend = c(data$ymax, data$ymin),
+    #   alpha = c(NA_real_, NA_real_),
+    #   !!!common,
+    #   .size = 2
+    # )
+    # whiskers <- flip_data(whiskers, flipped_aes)
 
     box <- data_frame0(
         xmin = data$xmin,
@@ -185,32 +274,32 @@ GeomQuantileplot <- ggproto("GeomQuantileplot", Geom,
     )
     box <- flip_data(box, flipped_aes)
 
-    if (!is.null(data$outliers) && length(data$outliers[[1]] >= 1)) {
-      outliers <- data_frame0(
-        y = data$outliers[[1]],
-        x = data$x[1],
-        colour = outlier.colour %||% data$colour[1],
-        fill = outlier.fill %||% data$fill[1],
-        shape = outlier.shape %||% data$shape[1],
-        size = outlier.size %||% data$size[1],
-        stroke = outlier.stroke %||% data$stroke[1],
-        #fill = NA,
-        alpha = outlier.alpha %||% data$alpha[1],
-        .size = length(data$outliers[[1]])
-      )
-      outliers <- flip_data(outliers, flipped_aes)
+    # if (!is.null(data$outliers) && length(data$outliers[[1]] >= 1)) {
+    #   outliers <- data_frame0(
+    #     y = data$outliers[[1]],
+    #     x = data$x[1],
+    #     colour = outlier.colour %||% data$colour[1],
+    #     fill = outlier.fill %||% data$fill[1],
+    #     shape = outlier.shape %||% data$shape[1],
+    #     size = outlier.size %||% data$size[1],
+    #     stroke = outlier.stroke %||% data$stroke[1],
+    #     #fill = NA,
+    #     alpha = outlier.alpha %||% data$alpha[1],
+    #     .size = length(data$outliers[[1]])
+    #   )
+    #   outliers <- flip_data(outliers, flipped_aes)
 
-      outliers_grob <- GeomPoint$draw_panel(outliers, panel_params, coord)
-    } else {
-      outliers_grob <- NULL
-    }
-
+    #   outliers_grob <- GeomPoint$draw_panel(outliers, panel_params, coord)
+    # } else {
+    #   outliers_grob <- NULL
+    # }
+    browser()
     ggname("geom_quantileplot", grobTree(
       #outliers_grob,
       #GeomSegment$draw_panel(whiskers, panel_params, coord, lineend = lineend),
       GeomCrossbar$draw_panel(
         box,
-        fatten = 0,
+        fatten = 2,
         panel_params,
         coord,
         lineend = lineend,

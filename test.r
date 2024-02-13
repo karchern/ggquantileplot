0247ffd3177477da6a37bda393ae7de3154e7121
Nicolai Karcher <karchernic@gmail.com>
2024-02-13 16:48:43 +0100

    implement > 2 groups

--- test.r
+++ test.r
@@ -9,6 +9,7 @@ library(ggquantileplot)
 baseColors <- c("OJ" = "#c45e5e", "VC" = "#2635a6")
 quantilesP <- c(0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
 
+
 quantilePlot <- ggplot(data = ToothGrowth, aes(x = as.factor(dose), y = len)) +
     # First entry of quantilesP needs to be 0.5
     geom_quantileplot(aes(fill = supp), quantilesP = quantilesP) +
